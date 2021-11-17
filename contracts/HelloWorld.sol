2186cbe3843378910950acad15ccc9a777639f5e
Paul McAviney <87715498+paul-mcaviney@users.noreply.github.com>
2021-11-16 17:24:59 -0700

    HelloWorld contract and run.js

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract HelloWorld {
+
+    //string public message;
+
+    constructor() {
+        console.log("Testing testing, is this thing on?");
+    }
+
+}
